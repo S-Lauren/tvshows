3bb81cffc366ecef5f580a47d8233016414ccb06
S-Lauren <s.lauren.contact@gmail.com>
2020-03-27 13:47:45 +0100

    conditionnal rendering and handle event onchange arrow

--- src/components/SeriesList/index.js
+++ src/components/SeriesList/index.js
@@ -10,7 +10,6 @@ const SeriesList = (props) => {
 
                 })}
             </ul>
-       
     )
 
 }
--- src/containers/Series/index.js
+++ src/containers/Series/index.js
@@ -5,11 +5,13 @@ class Series extends Component {
 
     // The State Object That Will Change after. 
     state = {
-        series: []
+        series: [],
+        seriesName: ''
     }
 
-    componentDidMount() {
-        fetch('http://api.tvmaze.com/search/shows?q=girls')
+    onChangeInput = e => {
+        this.setState({seriesName: e.target.value})
+        fetch(`http://api.tvmaze.com/search/shows?q=${e.target.value}`)
         .then(resp => resp.json())
         .then(data => {
             console.log(data);
@@ -21,12 +23,22 @@ class Series extends Component {
 
 
     render() {
-
+        const {series, seriesName} = this.state; 
         return (
             <div> 
                 <p>No Friends, I've Been Chillin' Alone, Fuck Nigga Don't Be hittin' My Phone !</p>
-                <input type="text" placeholder="search"/>
+                <input type="text" placeholder="search" onChange={this.onChangeInput} value={this.state.seriesName}/>
                 <SeriesList list = {this.state.series} />
+
+                {   
+                    series.length === 0 && seriesName.trim() === ''
+                    && <p>Please Enter the name of your favorite Serie</p>
+                }
+                {   
+                    series.length === 0 && seriesName.trim() !== ''
+                    && <p>No Matches found...</p>
+                }
+
             </div>
 
         )
